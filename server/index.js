bea9596bf6cabe0efa78cf3a481f8cf42bf3c3f0
andyoe0102 <oe.andy0102@gmail.com>
2020-06-24 18:54:37 -0500

    jest test on end points and seed generator

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.get('/api/reviews/:listingId',(req,res) =>{
         let length = data.length;
         let score=0;
 
-        console.log('ahhhh')
+
         data.forEach(review =>{
           score+=review.average;
         })
@@ -32,7 +32,7 @@ app.get('/api/reviews/:listingId',(req,res) =>{
       })
   }else{
     dbConnection.query(`SELECT * FROM reviews WHERE listingId = ${listingId}`, (err,data)=>{
-      console.log(data);
+      // console.log(data);
       res.json(data);
     })
   }
@@ -45,16 +45,11 @@ app.get('/api/suggestions/:listingId',(req,res) =>{
   const listingId = req.params.listingId;
   let listArr=[Number(listingId)];
 
-  // let lists = [];
-  // for(let i = 0 ; i<100;i++){
-  //   lists.push(i)
-  // };
-
   let lists = new Array(100).fill(null).map((ele,idx) =>{return idx})
 
   lists.splice(listingId,1);
 
-  for(var j =0; j<12;j++){
+  for(var j =0; j<11;j++){
     var random = lists[Math.floor(Math.random() * lists.length)];
     listArr.push(random);
     var index = lists.indexOf(random)
@@ -65,7 +60,7 @@ app.get('/api/suggestions/:listingId',(req,res) =>{
   listArr.forEach(listId =>{
     result.push(new Promise((resolve,reject)=>{
       dbConnection.query(`SELECT * FROM suggestions WHERE listingId=${listId}`,(err,listObj)=>{
-        console.log(listObj)
+        // console.log(listObj)
         resolve(listObj[0])
       })
     }))
@@ -82,6 +77,4 @@ app.get('/api/suggestions/:listingId',(req,res) =>{
 })
 
 
-app.listen(PORT, () => {
-  console.log(`App is now listening on port ${PORT}`);
-});
+module.exports.app = app;
---
+++ server/server.js
@@ -0,0 +1,5 @@
+const {app} = require('./index.js')
+
+app.listen(3000, () => {
+  console.log(`App is now listening on port 3000`);
+});
