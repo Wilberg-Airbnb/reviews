670a49de5681022ab928e6886f4b1b67500b1fed
andyoe0102 <oe.andy0102@gmail.com>
2020-06-26 19:15:12 -0500

    fix enzyme setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  setupFilesAfterEnv: ['./jest.setup.js'],
+  verbose:true,
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+  },
+  testTimeout:20000000,
+  setupFilesAfterEnv : ["<rootDir>/test/setupTests.js"],
+  transform:{
+    "^.+\\.jsx?$": "babel-jest"
+  }
 };
