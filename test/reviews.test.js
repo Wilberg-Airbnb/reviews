7af7ae8306ac0cb4551b4cc85a33173323aa519b
andyoe0102 <oe.andy0102@gmail.com>
2020-07-23 14:17:40 -0500

    fix jest issue with circleci

--- test/reviews.test.js
+++ test/reviews.test.js
@@ -1,12 +1,17 @@
 
 const request = require('supertest')
 const {app} = require('../server/index.js');
-// const {generateSeed,generateSeed2} = require('../server/seed.js')
+// const {generateSeed,generateSeed2} = require('../server/seed.js');
+const {dbConnection} = require('../MYSQL/index.js');
 
 
 
 
 xdescribe('dummy data generation seed Test',() =>{
+
+
+  afterAll(()=>{ dbConnection.connection.end();});
+
 
   test('generate reviews information for 100 listingId',async() =>{
     const reviewSeed = await generateSeed();
@@ -24,29 +29,37 @@ xdescribe('dummy data generation seed Test',() =>{
 
 describe('server endpoints Test',()=>{
 
-  test("it should get an array of reviews for endpoint '/api/reviews/:listingId'", async()=>{
+  afterAll(()=>{ dbConnection.connection.end();});
+
+  test("it should get an array of reviews for endpoint '/api/reviews/:listingId'", async(done)=>{
     const response = await request(app).get("/api/reviews/13");
     expect(JSON.parse(response.text)).toEqual(expect.any(Array))
-    expect(JSON.parse(response.text)[0].listingId).toEqual(13)
+    expect(JSON.parse(response.text)[0].listingId).toEqual(13);
+
+    done();
+
   })
 
-  test("it should get an average score of review for the listingId when endpoint query contains type=review", async()=>{
+  test("it should get an average score of review for the listingId when endpoint query contains type=review", async(done)=>{
 
     const response = await request(app).get("/api/reviews/2?type=review");
 
     expect(JSON.parse(response.text)).toEqual(expect.any(Number))
+    done();
   })
 
-  xtest("it should give an array length of 12 with each index consisting of suggested listingId information", async() =>{
+  xtest("it should give an array length of 12 with each index consisting of suggested listingId information", async(done) =>{
     const response = await request(app).get("/api/suggestions/15");
 
     expect(JSON.parse(response.text).length).toEqual(12);
-    expect(JSON.parse(response.text)[0].listingId).toEqual(expect.any(Number))
+    expect(JSON.parse(response.text)[0].listingId).toEqual(expect.any(Number));
+    done();
   })
 
-  test("it should get average review score for array of listIds requested as req.query.array", async() =>{
+  test("it should get average review score for array of listIds requested as req.query.array", async(done) =>{
     const response = await request(app).get("/api/reviews/?array=[1,2]");
-    expect(JSON.parse(response.text)).toEqual(expect.any(Number))
+    expect(JSON.parse(response.text)).toEqual(expect.any(Number));
+    done();
   })
 })
 
