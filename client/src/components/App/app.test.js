312dc885f6fbbe8dc72079583cab2776f2a681b1
andyoe0102 <oe.andy0102@gmail.com>
2020-07-27 18:04:58 -0500

    add puppeteer tests and clean up codes

--- client/src/components/App/app.test.js
+++ client/src/components/App/app.test.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {mount,shallow,configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+const puppeteer = require('puppeteer');
+
 
 import axios from 'axios';
 
 import App from './app.jsx';
 import Modal from '../Modal/Modal.jsx';
+import Profile from '../Profile/Profile.jsx';
 
 configure({adapter:new Adapter()});
 
@@ -45,7 +48,73 @@ describe('App component',()=>{
 
     wrapper.find('.modalButton').simulate('click');
     expect(wrapper.state('modalOpen')).toBe(true);
-  })
+  });
+
+  it('Can open and close modal', async () => {
+    let browser = await puppeteer.launch({
+      headless: false,
+      devtools: true,
+      slowMo: 250
+    });
+    let page = await browser.newPage();
+
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 900
+      },
+      userAgent: ''
+    });
+
+    async function autoScroll(page){
+      await page.evaluate(async () => {
+          await new Promise((resolve, reject) => {
+              var totalHeight = 0;
+              var distance = 100;
+              var timer = setInterval(() => {
+                  var scrollHeight = document.body.scrollHeight;
+                  window.scrollBy(0, distance);
+                  totalHeight += distance;
+
+                  if(totalHeight >= scrollHeight){
+                      clearInterval(timer);
+                      resolve();
+                  }
+              }, 500);
+          });
+      });
+    }
+
+    await page.goto('http://localhost:8080/12');
+    await page.waitForSelector('.modalButton');
+    await page.click("#review > div > button");
+    await autoScroll(page);
+    await page.click('#closebox');
+    browser.close();
+  }, 9000000);
+
+  it('Can click readmore to see full comment', async () => {
+    let browser = await puppeteer.launch({
+      headless: false,
+      devtools: true,
+      slowMo: 250
+    });
+    let page = await browser.newPage();
+
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 900
+      },
+      userAgent: ''
+    });
+
+    await page.goto('http://localhost:8080/12');
+
+    await page.click("#review > div > div.sc-fznxsB.cUWXFh > div:nth-child(1) > div.sc-AxhUy.fxWvvr.comments > span");
+
+    browser.close();
+  }, 9000000);
 
 
 })
--- test/reviews.test.js
+++ test/reviews.test.js
@@ -14,11 +14,7 @@ xdescribe('dummy data generation seed Test',() =>{
     expect(reviewSeed[0][0].listingId).toEqual(0);
   });
 
-  test('generate information for 100 listingIds for place suggestions', async() =>{
-    const suggestionSeed = await generateSeed2();
-    expect(suggestionSeed.length).toEqual(100);
-    expect(suggestionSeed[0].placeName).toEqual(expect.any(String))
-  })
+
 
 });
 
@@ -37,12 +33,7 @@ describe('server endpoints Test',()=>{
     expect(JSON.parse(response.text)).toEqual(expect.any(Number))
   })
 
-  test("it should give an array length of 12 with each index consisting of suggested listingId information", async() =>{
-    const response = await request(app).get("/api/suggestions/15");
 
-    expect(JSON.parse(response.text).length).toEqual(12);
-    expect(JSON.parse(response.text)[0].listingId).toEqual(expect.any(Number))
-  })
 
   test("it should get average review score for array of listIds requested as req.query.array", async() =>{
     const response = await request(app).get("/api/reviews/?array=[1,2]");
